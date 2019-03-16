3ccbd25ade9ef5ba70c3e234121fca9d4d8bd78f
Kenton Lam <kenton_lam@outlook.com>
2019-03-16 11:46:05 +1000

    Use dict. Print better. Specify communication.

--- ass_2.py
+++ ass_2.py
@@ -1,12 +1,12 @@
 from gurobipy import *
 
-def assignment_2(): 
+def assignment(comm=2): 
     model = Model('WonderMarket Model 1')
 
     # set of store numbers
-    S = range(10)
+    S = [f'S{i}' for i in range(10)]
     # set of distribution centre numbers
-    D = range(3)
+    D = [f'DC{i}' for i in range(3)]
 
     # matrix of costs of transporting 1 truckload from d to s
     # indexed as C[d][s]
@@ -17,32 +17,39 @@ def assignment_2():
     ]
 
     # maximum capacity at each distribution centre.
-    Capacity = [72, 76, 40]
+    M = dict(zip(D, [72, 76, 40]))
 
     # vector of required truckloads at each store.
-    R = [18, 7, 21, 15, 17, 10, 6, 8, 7, 7]
+    R = dict(zip(S, [18, 7, 21, 15, 17, 10, 6, 8, 7, 7]))
     
     # matrix of truckloads from each DC to each store.
     # indexed as X[d][s]
     X = model.addVars(D, S, obj=C, name='X')
 
     # adds constraints.
-    # 1. required truckloads at each store.
+    # comm 1: required truckloads at each store.
     model.addConstrs(X.sum('*', s) >= R[s] for s in S)
-    # 2. maximum capacity at each distribution centre.
-    model.addConstrs(X.sum(d, '*') <= Capacity[d] for d in D)
+
+    # comm 2: maximum capacity at each distribution centre.
+    if comm >= 2:
+        model.addConstrs(X.sum(d, '*') <= M[d] for d in D)
     
     # minimise total cost of transport.
     model.modelSense = GRB.MINIMIZE
 
     model.optimize()
 
+    print()
+    print(f'Optimised for communication {comm}.')
+    print('Objective value:', model.objVal)
+    print()
     for s in S:
         for d in D:
             v = X[d, s]
             # print all non-zero variables.
             if v.x:
-                print(f'DC{d} to S{s}', v.varName, v.x)
+                print(f'{v.varName} ', v.x)
+        print()
 
 if __name__ == "__main__":
-    assignment_2()
+    assignment(2)
