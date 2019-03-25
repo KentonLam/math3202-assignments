11dbd73de503344b11ea6b302bfa60983122419e
Kenton Lam <kenton_lam@outlook.com>
2019-03-25 20:40:23 +1000

    Add gurobi_pprint. Tweak.

--- assignment_model.py
+++ assignment_model.py
@@ -1,7 +1,5 @@
 from gurobipy import *
-from collections import defaultdict
-
-import gurobi_pprint
+from collections import defaultdict, namedtuple
 
 def table(row_format, rows):
     s = ''
@@ -65,12 +63,15 @@ SurgeMultipliers = tupledict(
 
 def run_assignment_model(comm: int):
     global Surges, SurgeDemands, SurgeMultipliers
-    model = Model('WonderMarket Model')
+    assert 1 <= comm <= 4, "Invalid communication number."
     if comm < 4:
+        # for backwards compatibility with previous communications.
         Surges = ['none']
         SurgeDemands = {('none', s): Demands[s] for s in Stores}
         SurgeMultipliers = defaultdict(lambda: 1)
 
+    # the gurobi model.
+    model = Model('WonderMarket Model')
 
     # matrix of truckloads from each DC to each store during each surge.
     # indexed as X[d,s,u].
@@ -143,11 +144,11 @@ def run_assignment_model(comm: int):
     x_ = {(d,s,u): X[d,s,u] for u in Surges for s in Stores for d in DCs}
     y_ = {(d, s): Y[d,s] for s in Stores for d in DCs}
 
-    gurobi_pprint.print_variable_analysis(x_)
-    gurobi_pprint.print_variable_analysis(y_)
-    gurobi_pprint.print_variable_analysis(Z)
+    print_variable_analysis(x_)
+    print_variable_analysis(y_)
+    print_variable_analysis(Z)
 
-    gurobi_pprint.print_constr_analysis(constrs)
+    print_constr_analysis(constrs)
     
     print()
     print_assignments(Y)
@@ -160,11 +161,71 @@ def print_assignments(Y):
         fractions = map(lambda x: round(x, 4) if x else '', fractions)
         print('{:>5} | {:>6} {:>6} {:>6}'.format(s, *fractions))
 
-
-if __name__ == "__main__":
+# entry point of application.
+def main():
     from sys import argv
     if len(argv) < 2:
         comm = 4
     else:
         comm = int(argv[1])
     run_assignment_model(comm)
+
+
+# helper functions to print constraint and variable analysis.
+
+#region gurobi_pprint
+TableSpec = namedtuple('TableSpec', 'title header format generator')
+
+r = lambda i: round(i, 4)
+row_generator = {
+    'constraints': TableSpec(
+        'Constraint Analysis', 
+        ('constr', '', 'rhs', 'slack', 'pi', 'rhs low', 'rhs high'),
+        '  {:>1} {:>5} | {:>6} {:>6} | {:>7} {:>7}', 
+        lambda n, c: (n, c.sense, r(c.rhs), r(c.slack), r(c.pi), r(c.SARHSLow), r(c.SARHSUp))
+    ),
+    'variables': TableSpec(
+        'Variable Analysis', 
+        ('variable', 'x', 'coeff', 'rc', 'obj low', 'obj high'),
+        '  = {:>5} * {:>6} | {:>6} | {:>7} {:>7}', 
+        lambda n, c: (c.varName, r(c.x), r(c.obj), r(c.rc), r(c.SAObjLow), r(c.SAObjUp))),
+}
+
+def _dict_to_rows(constrs_list, generator, prefix=''):
+    if not isinstance(constrs_list, dict):
+        return [generator(prefix, constrs_list)]
+    if prefix:
+        p = prefix + ' '
+    else:
+        p = ''
+    out = []
+    for k, v in constrs_list.items():
+        rows = _dict_to_rows(v, generator, p+str(k))
+        out.extend(rows)
+    return out
+
+def _print_analysis(constr_dict, mode):
+    rows = _dict_to_rows(constr_dict, row_generator[mode].generator)
+    max_lens = []
+    for i in range(len(rows[0])):
+        this_max_len = max(map(lambda a: len(str(a)), (r[i] for r in rows)))
+        if i > 0:
+            this_max_len = min(this_max_len, 10)
+        max_lens.append(this_max_len)
+    
+    f_str = f'{{:>{max_lens[0]}}}' + row_generator[mode].format
+
+    print(row_generator[mode].title)
+    print(f_str.format(*row_generator[mode].header))
+    for r in rows:
+        print(f_str.format(*r))
+    
+def print_constr_analysis(constrs):
+    return _print_analysis(constrs, 'constraints')
+
+def print_variable_analysis(variables): 
+    return _print_analysis(variables, 'variables')
+#endregion
+
+if __name__ == "__main__":
+    main()
